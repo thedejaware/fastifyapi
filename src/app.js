df24f3d9432394096d3afa8657660cc6048819f6
thedejaware <mehmet.akin84@gmail.com>
2019-06-09 22:46:02 +0300

    - Routes have been submitted

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const fastify = require('fastify')({
     logger: true
 })
 
+// Rquire routes
+const routes = require('./routes/cars.route');
 // Require mongoose
 const mongoose = require('mongoose');
 
@@ -11,6 +13,11 @@ mongoose.connect('mongodb://localhost/mycargarage')
 .then(() => console.log('MongoDB connected...'))
 .catch(err => console.log(err));
 
+
+// Looping over all routes
+routes.forEach((route, index) => {
+    fastify.route(route);
+});
 // Routing
 fastify.get('/', async (request, response) => {
     return { status: 'Server is running' }
